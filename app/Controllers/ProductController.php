093fd4dbc61142b888a6c71e199773c694748f87
Azzahra Kareena <azzahrakareena30@student.uns.ac.id>
2023-10-20 10:57:32 +0700

    API

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -2,8 +2,12 @@
 namespace App\Controllers;
 use App\Controllers\BaseController;
 use App\Models\ProductModel;
+use CodeIgniter\API\ResponseTrait;
+use CodeIgniter\HTTP\RequestTrait;
 
 class ProductController extends BaseController {
+
+    use ResponseTrait;
 
     public function __construct(){
         $this->product = new ProductModel();
@@ -31,6 +35,17 @@ class ProductController extends BaseController {
         return view('product', $data);
     }
 
+    public function readProductsApi(){
+        $products = $this->product->findAll();
+
+        return $this->respond([
+            'code' => 200,
+            'status' => 'OK',
+            'data' => $products
+        ]
+        );
+    }
+
     public function getProduct($id){
         $product = $this->product->where('id', $id)->first();
         $data = [
@@ -38,6 +53,24 @@ class ProductController extends BaseController {
         ];
         return view('edit_product', $data);
     }
+
+    public function getProductApi($id){
+        $product = $this->product->where('id', $id)->first();
+        if(!$product){
+            $this->response->setStatusCode(404);
+            return $this->response->setJSON([
+                'code' => 404,
+                'status' => 'Not Found',
+                'data' => null
+            ]);
+        }
+        return $this->response->setJSON([
+            'code' => 200,
+            'status' => 'OK',
+            'data' => $product
+        ]);
+    }
+
     public function updateProduct($id){
         $nama_product = $this->request->getVar('nama_product');
         $description = $this->request->getVar('description');
